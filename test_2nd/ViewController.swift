0b7c61d34d34936b1a0ebe5df648eabe088f0cdc
huangyushin <rita82887@gmail.com>
2020-07-13 11:45:42 +0800

    Initial Commit

---
+++ test_2nd/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_2nd
+//
+//  Created by Rita on 2020/7/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
